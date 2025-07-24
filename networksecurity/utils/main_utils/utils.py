96603e975b21198265b1e28f7322747826074c7e
broaderai23 <manismansuri24@gmail.com>
2025-07-24 20:11:07 +0530

    deployment

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import GridSearchCV
 def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
-            return yaml.safe_load(yaml_file)
+            return yaml.safe_load(yaml_file) 
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
     
